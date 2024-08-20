4f72ed1dcbe4df9e308b548952e83b5487c7cfea
Francisca Robles <flanna.jpg@gmail.com>
2024-08-20 16:15:32 -0400

    first commit

--- src/main/java/servlet/ConsultaFactorial.java
+++ src/main/java/servlet/ConsultaFactorial.java
@@ -12,15 +12,10 @@ import java.io.IOException;
 public class ConsultaFactorial extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ConsultaFactorial() {
         super();
         // TODO Auto-generated constructor stub
     }
-
-
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // TODO Auto-generated method stub
@@ -39,7 +34,6 @@ public class ConsultaFactorial extends HttpServlet {
         response.getWriter().append("El factorial de " + num + " es " + factorial);
 
     }
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         // TODO Auto-generated method stub
